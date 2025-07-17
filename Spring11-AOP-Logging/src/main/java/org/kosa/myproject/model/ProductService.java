a041e24a422e17d7aa63bd61721d8b124d7f0e04
GIS-BM <gogointospace@gmail.com>
2025-07-17 11:39:50 +0900

    feat: aop

---
+++ Spring11-AOP-Logging/src/main/java/org/kosa/myproject/model/ProductService.java
@@ -0,0 +1,24 @@
+package org.kosa.myproject.model;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+  // 공통 로그 컴포넌트를 사용하기 위해 객체 생성한다.
+  
+  public void updateProduct(String anem, String maker, int price) {
+    System.out.println(getClass().getName()+" core concern updateProduct");
+  }
+  
+  public void findProductById(String id) {
+    System.out.println(getClass().getName()+" core concern findProductById");
+  }
+  
+  public void findProductByName(String name) {
+    System.out.println(getClass().getName()+" core concern findProductByName");
+  }
+  
+  public void findProductByMaker(String maker) {
+    System.out.println(getClass().getName()+" core concern findProductByMaker");
+  }
+}
